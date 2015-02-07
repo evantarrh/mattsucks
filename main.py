17fd09ad697fd5c92bc6b46b01273c348b51d7d4
evantarrh <evantarrh@gmail.com>
2015-02-07 05:00:06 -0500

    change of heart pretty well implemented now

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ app.config["DEBUG"] = True
 account_sid = config.acct_sid
 auth_token  = config.token
 client = TwilioRestClient(account_sid, auth_token)
+
+
  
-
-
-
-
 @app.route('/')
 def hello():
 	return render_template("index.html")
 
-@app.route('/servo_post', methods=["POST"])
-def servo_pos():
+@app.route('/mattsucks', methods=["POST"])
+def mattsucks():
 	message = client.messages.create(body="Hey Matt! You suck.",
 	to = config.matts_num,
-	from_ = "+16179350930")
+	from_ = config.twilio_num)
 	print message.sid
 	return "You told Matt he sucks!"
+
+@app.route('/mattrocks', methods=["POST"])
+def mattrocks():
+	message = client.messages.create(body="Aw geez! Sorry Matt. I hope all of your dreams come true.",
+	to = config.matts_num,
+	from_ = config.twilio_num)
+	print message.sid
+	return "Aw, that's nice."
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0")
--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,74 @@
 
-var testimonials = ["\"I've been shitting on Matt for years. He sucks.\" - Sahir",
+var bad_testimonials = ["\"I've been shitting on Matt for years. He totally sucks.\" - Sahir",
 	"\"I only met him once, but yeah, Matt blows chunks.\" - Nate",
 	"\"Please stop.\" - Matt",
-	"\"Sorry Matt. You do suck.\" - Evan"]
+	"\"Sorry Matt. But you do suck.\" - Evan",
+	"\"Having a second kid was a mistake.\" - Matt's dad",
+	"\"Matt is a huge weenie, and we all know it\" - Jaden Smith",
+	"\"Shut up, Matt.\" - Tom Hanks",
+	"\"I visited Columbia yesterday, and Matt is the reason I'm not coming here.\" - Malia Obama",
+	"\"Matt has a lot of opinions, and all of them are bad.\" - Eunice"
+	];
+
+var nice_testimonials = ["\"Actually Matt is cool\" - one guy",
+	"\"Actually Matt is cool\" - another guy",
+	"\"Actually Matt is cool\" - some dweeb"
+	];
+
+var howManyTimesHaveWeShatOnMatt = 0;
 
 $().ready(function() {
 
+	// $("#matt-rocks").click(function(e) {
+
+	// 	console.log("Matt totally rocks!");
+
+	// 	$.post("/mattrocks");
+	// })
+
 	$("#matt-sucks").click(function(e) {
-
-		console.log("Matt sucks a whole lot, you are doing god's work");
-
-		$.post("/servo_post");
-
+		if (howManyTimesHaveWeShatOnMatt < 3) {
+			//$.post("/mattsucks");
+		}
+		else if (howManyTimesHaveWeShatOnMatt === 3) {
+			console.log($("#matt-sucks").css("display"));
+			// $("#matt-sucks").css("display", "hidden");
+			// $("#matt-rocks").css("display", "visible");
+			$("h1").animate({'opacity': 0}, 1000, function() {
+				$(this).text("Matt rocks");
+			}).animate({"opacity": 1}, 1000);
+			$("#matt-sucks").animate({'opacity': 0}, 1000, function() {
+				$(this).text("No wait! Tell Matt he rocks.");
+			}).animate({"opacity": 1}, 1000);
+			//changeOfHeart();
+		}
+		else if (howManyTimesHaveWeShatOnMatt > 3 && howManyTimesHaveWeShatOnMatt < 6) {
+			$.post("/mattrocks");
+		}
+		howManyTimesHaveWeShatOnMatt++;
 	});
 
+	// changeOfHeart = function() {
+	// 	console.log("hi");
+	// 	$("#matt-sucks").css("display", "hidden");
+	// 	$("#matt-rocks").css("display", "visible");
+	// 	$("h1").animate({'opacity': 0}, 1000, function() {
+	// 		$(this).text("Matt rocks");
+	// 	}).animate({"opacity": 1}, 1000);
+
+	// }
+
 	setInterval(function() {
-		$("#testimonial").animate({'opacity': 0}, 1000, function(){
-			$(this).text(testimonials[ Math.floor(Math.random() * testimonials.length) ]);
-		})
-		.animate({'opacity': 1}, 1000);
+		if (howManyTimesHaveWeShatOnMatt < 3) {
+			$("#testimonial").animate({'opacity': 0}, 1000, function(){
+				$(this).text(bad_testimonials[ Math.floor(Math.random() * bad_testimonials.length) ]);
+			}).animate({'opacity': 1}, 1000);
+		}
+		else {
+			$("#testimonial").animate({'opacity': 0}, 1000, function(){
+				$(this).text(nice_testimonials[ Math.floor(Math.random() * nice_testimonials.length) ]);
+			}).animate({'opacity': 1}, 1000);			
+		}
 	}, 4000);
 
 
