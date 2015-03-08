2c00a4162b840e4e1c6c6a986de30db1951c47b0
evantarrh <evantarrh@gmail.com>
2015-03-08 18:02:18 -0400

    more improvements to form, trying to fix button animation

--- static/js/main.js
+++ static/js/main.js
@@ -9,18 +9,7 @@ var bad_testimonials = ["\"I've been hating on Matt for years. He totally sucks.
 	"\"Matt? What a bag.\" - All of the judges"
 	];
 
-var nice_testimonials = ["\"Actually Matt is super cool\" - some guy probably",
-	"\"Oh, Matt? Matt has sick references.\" - Rasmi",
-	"\"Most of the time, Matt doesn't suck.\" - Yih-Jen",
-	"\"Matt has killer dance moves.\" - Brian",
-	"\"Matt is a brilliant person.\" - Evan after Matt beat him at Trivia Crack",
-	"\"I freaking love that guy.\" - Dan",
-	"\"His presence shines radiance.\" - Lil B",
-	"\"Matt knows what to do. All the time.\" - W3Schools"
-	];
-
-
-var subheadReset;
+var buttonReset;
 var numclicks = 0;
 
 var data = document.querySelector('#data');
@@ -28,25 +17,29 @@ console.dir(data);
 var first_name = data.dataset.first_name;
 var urlstring = data.dataset.urlstring;
 
-
-
 $().ready(function() {
 
 	$("#matt-sucks").click(function(e) {
-		window.clearInterval(subheadReset);
+		window.clearInterval(buttonReset);
 		if(numclicks >= 3){
-			$('h2').text("Cool your jets");
+			$('button').text("Cool your jets");
 		}else{
 			$.post("/sendtext/" + urlstring , function( response ){
 				console.dir(response);
-			})
+			});
+			
+			$("button").fadeIn(function() {
+				$(this).text("Nice! You told Matt he sucks.");
+				$(this).css("color", "#48d");
+			});
+			buttonReset = setTimeout(function() {
+				$("button").fadeIn(function() {
+					$(this).text("Tell Matt he sucks.");
+					$(this).css("color", "#87cefa");
+				});
+			}, 4000);
 		}
 	});
-
-
-
-
-
 
 	setInterval(function() {
 		$("#testimonial").animate({'opacity': 0}, 1000, function(){
